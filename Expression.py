6be555dfdfca53cf609c8cd7a7ccc2752d43e3a0
dsubbs <danyasutts@gmail.com>
2019-02-19 23:58:13 +0200

    expression enter pt 2
    Exit command, postcodes to debug, temporal scope, printing scoped variable, multiplication (fix required), command container mustn't be int.

--- Expression.py
+++ Expression.py
@@ -5,15 +5,62 @@ class Expression:
         self.local_vars = local_vars
 
     def Render(self):
+
+        # Quitting the application, throws 1 if "exit"
+        if (self.expression == "exit"):
+            return 1
+
+        # If there is no equality sign, checks if the expression is in the scope.
+        # If it is, print it, if not - alert the user.
         if not ('=' in self.expression):
+            temp = self.local_vars.get(self.expression)
+            if not temp == None:
+                print(temp)
+            else:
+                print("There is no variable " + self.expression + " in the scope")
+            return self.local_vars
+
+        var_name = self.expression[:self.expression.find('=') - 1]
+
+        # Check if the variable name is a number.
+        # If so, raise an error, pass otherwise.
+        try:
+            temp = int(var_name)
+            raise WrongExpression
+        except ValueError:
+            print("var name is legal")
+            pass
+        except WrongExpression:
+            print("var name can't be integer")
             raise WrongExpression
 
-        if ('*' in self.expression):
-        # Render the multiplication
-            pass
+        # Assume expression assigns integers
+        var_cont = self.expression[self.expression.find('=') + 1:].strip()
+
+        print("checking for operations")
+
+        result = 1
+
+        if ('*' in var_cont):
+            print("found a multiplication")
+            vars = []
+            while ('*' in var_cont):
+                vars.append(var_cont[:var_cont.find('*') - 1])
+                var_cont = var_cont[var_cont.find('*') + 1:]
+            print(vars)
+            for elem in vars:
+                if self.local_vars.get(elem) == None:
+                    raise WrongExpression
+                else:
+                    result = result * self.local_vars.get(elem)
+
+        if (not '*' in self.expression) and (not '+' in self.expression):
+            result = int(var_cont)
 
         if ('=' in self.expression):
-            var_name = self.expression[:self.expression.find('=') - 1]
-            # Assume expression assigns integers
-            var_cont = int(self.expression[self.expression.find('=') + 1:])
-            self.local_vars.update({var_name: var_cont})
+            print("creating new variable")
+            self.local_vars.update({var_name: result})
+            return self.local_vars
+
+        # If nothing was done at the end, return 2.
+        return 2
--- la-calculator.py
+++ la-calculator.py
@@ -1,4 +1,4 @@
-import math
+import math, os
 from Expression import Expression
 
 local_vars = {}
@@ -6,8 +6,21 @@ local_vars = {}
 while True:
     try:
         text = input()
-        expr = Expression(text, local_vars)
-        expr.Render()
+
+        local_vars_temp = local_vars
+        expr = Expression(text, local_vars_temp)
+        local_vars_temp = expr.Render()
+
+        # Check for traceback codes and render them.
+        if local_vars_temp == 1:
+            os._exit(1)
+        elif local_vars_temp == 2:
+            print("Something went wrong with the code.")
+            raise WrongExpression
+        else:
+            local_vars = local_vars_temp
+
         print(local_vars)
-    except:
+    except Exception as exception:
+        print(type(exception).__name__)
         print("Illegal expression, try again")
